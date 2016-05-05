88f0ac7c42e75e3e8166ea2503d0b2f3c896de84
Gordon Diggs <gordon@gordondiggs.com>
2016-05-05 11:04:55 -0400

    so true

--- cool.py
+++ cool.py
@@ -1,3 +1,53 @@
+def complex():
+    if true:
+        if true:
+            if true:
+                if true:
+                    if true:
+                        if true:
+                            if true:
+                                if true:
+                                    if true:
+                                        if true:
+                                            if true:
+                                                if true:
+                                                    if true:
+                                                        if true:
+                                                            if true:
+                                                                if true:
+                                                                    if true:
+                                                                        if true:
+                                                                            if true:
+                                                                                if true:
+                                                                                    if true:
+                                                                                        if true:
+                                                                                            if true:
+                                                                                                return "hi"
+def complex2():
+    if true:
+        if true:
+            if true:
+                if true:
+                    if true:
+                        if true:
+                            if true:
+                                if true:
+                                    if true:
+                                        if true:
+                                            if true:
+                                                if true:
+                                                    if true:
+                                                        if true:
+                                                            if true:
+                                                                if true:
+                                                                    if true:
+                                                                        if true:
+                                                                            if true:
+                                                                                if true:
+                                                                                    if true:
+                                                                                        if true:
+                                                                                            if true:
+                                                                                                return "hi2"
 def foo():
   '''
   exclude me
